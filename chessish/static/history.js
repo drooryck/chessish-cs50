cac5d64abfe9f1814d2ae3803bbc9743397e0ac8
Dries P. Rooryck <44844091+drooryck@users.noreply.github.com>
2023-01-13 18:16:56 +0100

    Delete chessish directory

--- chessish/static/history.js
+++
@@ -1,2 +0,0 @@
-var ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
-var board = Chessboard('myBoard', ruyLopez)
--- chessish/stock.py
+++
@@ -1,4 +0,0 @@
-# This python program imports Stockfish, an open-source Chess Artificial Intelligence widely recognized to be among the strongest.
-import os
-from stockfish import Stockfish
-stockfish = Stockfish(path="/usr/local/python/3.10.4/lib/python3.10/site-packages")
