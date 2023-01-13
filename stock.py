1a48c556129eb41bb1339627630f1cf15c193c43
Dries P. Rooryck <44844091+drooryck@users.noreply.github.com>
2023-01-13 12:30:41 -0500

    Add files via upload
    All webapp files

---
+++ stock.py
@@ -0,0 +1,4 @@
+# This python program imports Stockfish, an open-source Chess Artificial Intelligence widely recognized to be among the strongest.
+import os
+from stockfish import Stockfish
+stockfish = Stockfish(path="/usr/local/python/3.10.4/lib/python3.10/site-packages")
